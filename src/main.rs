e8dda1e68def219b5b97b13da1da9e32624b1f21
Southball <jamie.choi.mail.2002@gmail.com>
2020-08-23 02:39:41 +0800

    Move to Glade and add features

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,112 @@
-extern crate gio;
-extern crate glib;
 extern crate gtk;
 
-use gio::prelude::*;
 use gtk::prelude::*;
 
-fn build_ui(application: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(application);
-    
-    window.set_title("Todo List");
-    window.set_border_width(10);
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_default_size(800, 600);
-
-    let grid = gtk::Grid::new();
-
-    let column_type = [String::static_type(), String::static_type()];
-    let title_column_id = 0i32;
-    let description_column_id = 1i32;
-
-    let list_store = gtk::ListStore::new(&column_type);
-
-    let title_input_buffer = gtk::EntryBuffer::new(None);
-    let description_input_buffer = gtk::EntryBuffer::new(None);
-    let title_input = gtk::EntryBuilder::new()
-        .placeholder_text("Title")
-        .buffer(&title_input_buffer)
-        .build();
-    let description_input = gtk::EntryBuilder::new()
-        .placeholder_text("Description")
-        .buffer(&description_input_buffer)
-        .build();
-    let add_button = gtk::ButtonBuilder::new()
-        .label("Add Todo")
-        .build();
-
-    let title_input_buffer_clone = title_input_buffer.clone();
-    let description_input_buffer_clone = description_input_buffer.clone();
-    let list_store_clone = list_store.clone();
-    add_button.connect_clicked(move |e| {
-        list_store_clone.insert_with_values(
-            None,
-            &[0, 1],
-            &[
-                &title_input_buffer_clone.get_text(),
-                &description_input_buffer_clone.get_text(),
-            ],
-        );
-    });
-
-    grid.attach(&title_input, 0, 1, 1, 1);
-    grid.attach(&description_input, 1, 1, 1, 1);
-    grid.attach(&add_button, 2, 1, 1, 1);
-    
-    let todo_view = gtk::TreeView::new();
-    let title_renderer = gtk::CellRendererText::new();
-    let description_renderer = gtk::CellRendererText::new();
-    let title_column = gtk::TreeViewColumn::new();
-    let description_column = gtk::TreeViewColumn::new();
-
-    todo_view.set_model(Some(&list_store));
-
-    title_column.set_title("Title");
-    title_column.pack_start(&title_renderer, true);
-    title_column.add_attribute(&title_renderer, "text", title_column_id);
-    title_column.set_sort_column_id(title_column_id);
-    title_column.set_sizing(gtk::TreeViewColumnSizing::Autosize);
-    title_column.set_resizable(true);
-    description_column.set_title("Description");
-    description_column.pack_start(&description_renderer, true);
-    description_column.add_attribute(&description_renderer, "text", description_column_id);
-    description_column.set_sort_column_id(description_column_id);
-    description_column.set_sizing(gtk::TreeViewColumnSizing::Autosize);
-    description_column.set_resizable(true);
-        
-    todo_view.append_column(&title_column);
-    todo_view.append_column(&description_column);
-
-    todo_view.set_hexpand(true);
-    todo_view.set_vexpand(true);
-
-    grid.attach(&todo_view, 0, 0, 3, 1);
-
-    window.add(&grid);
-    window.show_all();
-}
+const GLADE_SRC: &'static str = include_str!("./ui.glade");
 
 fn main() {
-    let application =
-        gtk::Application::new(Some("cc.southball.gtk.todo_list"), Default::default())
-            .expect("Failed initialization.");
+    gtk::init().expect("Failed to initialize GTK.");
 
-    application.connect_activate(|app| {
-        build_ui(app);
+    let builder = gtk::Builder::from_string(GLADE_SRC);
+
+    let window: gtk::ApplicationWindow = builder.get_object("window-main").unwrap();
+
+    let store_types = [String::static_type(), String::static_type()];
+    let title_column_id = 0;
+    let description_column_id = 1;
+    let store = gtk::ListStore::new(&store_types);
+
+    // Handle TreeView
+    {
+        let treeview_todo: gtk::TreeView = builder.get_object("treeview-todo").unwrap();
+
+        let title_cell = gtk::CellRendererText::new();
+        let description_cell = gtk::CellRendererText::new();
+
+        let title_column = gtk::TreeViewColumnBuilder::new()
+            .title("Title")
+            .sizing(gtk::TreeViewColumnSizing::Autosize)
+            .resizable(true)
+            .sort_column_id(title_column_id)
+            .build();
+        let description_column = gtk::TreeViewColumnBuilder::new()
+            .title("Description")
+            .sizing(gtk::TreeViewColumnSizing::Autosize)
+            .resizable(true)
+            .sort_column_id(description_column_id)
+            .build();
+
+        title_column.pack_start(&title_cell, true);
+        description_column.pack_start(&description_cell, true);
+
+        title_column.add_attribute(&title_cell, "text", title_column_id);
+        description_column.add_attribute(&description_cell, "text", description_column_id);
+
+        treeview_todo.append_column(&title_column);
+        treeview_todo.append_column(&description_column);
+
+        treeview_todo
+            .get_selection()
+            .set_mode(gtk::SelectionMode::Multiple);
+        treeview_todo.set_model(Some(&store));
+    }
+
+    // Handle Adding To Do
+    let button_add_todo: gtk::Button = builder.get_object("button-add-todo").unwrap();
+    {
+        let store = store.clone();
+        button_add_todo.connect_clicked(move |_event| {
+            let store = store.clone();
+
+            let builder = gtk::Builder::from_string(GLADE_SRC);
+            let window: gtk::Window = builder.get_object("window-add-todo").unwrap();
+            let button_confirm_add_todo: gtk::Button =
+                builder.get_object("button-confirm-add-todo").unwrap();
+            let entry_title: gtk::Entry = builder.get_object("entry-title").unwrap();
+            let textview_description: gtk::TextView =
+                builder.get_object("textview-description").unwrap();
+
+            {
+                let window = window.clone();
+                button_confirm_add_todo.connect_clicked(move |_button| {
+                    let title = entry_title.get_text();
+                    let description = {
+                        let buffer = textview_description.get_buffer().unwrap();
+                        buffer
+                            .get_text(&buffer.get_start_iter(), &buffer.get_end_iter(), true)
+                            .unwrap()
+                    };
+                    store.insert_with_values(None, &[0, 1], &[&title, &description]);
+
+                    window.hide();
+                });
+            }
+
+            window.show_all();
+        });
+    }
+
+    // Handle Deleting Selection
+    let button_delete_selection: gtk::Button =
+        builder.get_object("button-delete-selection").unwrap();
+    {
+        let store = store.clone();
+        let treeview_todo: gtk::TreeView = builder.get_object("treeview-todo").unwrap();
+        button_delete_selection.connect_clicked(move |_button| {
+            let (paths, model) = treeview_todo.get_selection().get_selected_rows();
+            for path in paths.iter().rev() {
+                store.remove(&model.get_iter(path).unwrap());
+            }
+        });
+    }
+
+    // Handle Quitting Application
+    window.connect_delete_event(|_window, _event| {
+        gtk::main_quit();
+        Inhibit(true)
     });
 
-    application.run(&[]);
+    window.show_all();
+
+    gtk::main();
 }
